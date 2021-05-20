0e817d1c13a0e71b0025029091ca9e7e3fe4005f
Connor <connormccaskie@gmail.com>
2021-05-20 23:39:45 +1000

    Made rudimentary time-stop

--- ChrononTF/Assets/Scripts/Core.cs
+++ ChrononTF/Assets/Scripts/Core.cs
@@ -7,17 +7,47 @@ public class Core : MonoBehaviour
     public bool isTimeStopped;
     public float stoppedPeriod;
 
-    GameObject[] stoppableObjects;
+    public GameObject stoppables;
+
+    List<GameObject> stoppableObjects = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        stoppableObjects = (GameObject[])FindObjectsOfType(typeof(GameObject));
+        foreach(Transform stoppable in stoppables.transform)
+        {
+            stoppableObjects.Add(stoppable.gameObject);
+        }
+
+        if (isTimeStopped)
+        {
+            ToggleTime(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ToggleTime(!isTimeStopped);
+        }
+    }
+
+    /// <summary> 
+    ///activeness = true ? play time : stop time
+    /// </summary>
+    /// <param name="activeness"></param>
+    void ToggleTime(bool activeness)
+    {
+        foreach (GameObject gameObject in stoppableObjects)
+        {
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = !activeness;
+            }
+        }
+        isTimeStopped = activeness;
     }
 }
